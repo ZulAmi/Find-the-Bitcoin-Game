a5210b320da96441f1102e99a1b709f24f8bc0b0
Zulhilmi <negahim666@gmail.com>
2022-05-10 20:50:53 +0800

    latest update

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 const row = 10;
 const col = 10;
+const percentage = 0.1;
+const bitcoinpercentage = 0.01;
 
 class Field {
     field = [[]];
 
     constructor() {
-        //this.field = field;
         this.locationX = 0;
         this.locationY = 0;
         this.field[0][0] = pathCharacter;
@@ -24,17 +25,16 @@ class Field {
     }
 
     generateField() {
-        for (let y = 0; y < row; y++) {
-            for (let x = 0; x < col; x++) {
-                const prob = Math.random();
-                this.field[y][x] = prob > 0.1 ? fieldCharacter : hole;
+        for (let y = 0; y < col; y++) {
+            for (let x = 0; x < row; x++) {
+                const rand = Math.random();
+                this.field[y][x] = rand > percentage ? fieldCharacter : hole;
+                // this.field[y][x] = rand > bitcoinpercentage ;
+
             }
 
         }
-        const BitcoinLocation = {
-            x: Math.floor(Math.random() * row),
-            y: Math.floor(Math.random() * col)
-        }
+
 
     }
 
@@ -45,11 +45,17 @@ class Field {
             this.askQuestion();
             if (!this.inTheField()) {
                 playGame = false;
-                console.log('Game over. Transfer 1 Bitcoin to this address');
+                console.log('You left the field! Transfer 1 Bitcoin to continue!');
                 break;
-            } else if (this.isHole()) {
+            }
+            else if (this.isHole()) {
                 playGame = false;
-                console.log('Sorry, you fell down a hole!');
+                console.log('You fell down a hole! Transfer 1 Bitcoin to continue!');
+                break;
+            }
+            else if (this.isBitcoin()) {
+                playGame = false;
+                console.log('You won a bitcoin! We will transfer 1 Bitcoin to your wallet address!');
                 break;
             }
             this.field[this.locationX][this.locationY] = pathCharacter;
@@ -70,6 +76,10 @@ class Field {
         return this.field[this.locationY][this.locationX] === hole;
     }
 
+    isBitcoin() {
+        return this.field[this.locationY][this.locationX] === Bitcoin;
+    }
+
 
     print() {
         clear();
@@ -80,26 +90,25 @@ class Field {
     }
 
     askQuestion() {
-        const answer = prompt("FIND THE MISSING BITCOIN!!");
+        const answer = prompt('Find the Bitcoin ').toUpperCase();
         switch (answer) {
-            case 'w':
+            case 'W':
                 this.locationX -= 1;
                 break;
-            case 's':
+            case 'S':
                 this.locationX += 1;
                 break;
-            case 'a':
+            case 'A':
                 this.locationY -= 1;
                 break;
-            case 'd':
+            case 'D':
                 this.locationY += 1;
                 break;
             default:
-                console.log('Enter w, a, s or d.');
+                console.log('Enter W, A, S or D.');
                 this.askQuestion();
                 break;
         }
-
     }
 
     fieldArea() {
